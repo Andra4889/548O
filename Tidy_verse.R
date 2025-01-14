698a4bc161a19acfb5a7bd45c97a26988f8e01e8
Sandra Inusah <Andra4889>
2025-01-14 14:06:33 -0800

    tidying into a single data frame

---
+++ Tidy_verse.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+fship <- read_csv(file.path("data", "The_Fellowship_Of_The_Ring.csv"))
+ttow <- read_csv(file.path("data", "The_Two_Towers.csv"))
+rking <- read_csv(file.path("data", "The_Return_Of_The_King.csv")) 
+rking
+
+lotr_untidy <- bind_rows(fship, ttow, rking)
+str(lotr_untidy)
+lotr_untidy

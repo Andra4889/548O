79daec5b598a1527a97536bc04ad620abc02eff5
Sandra Inusah <Andra4889>
2025-01-30 14:25:27 -0800

    ifs and else. End of exercise three

--- Lecture-8.exercises.R
+++ Lecture-8.exercises.R
@@ -69,6 +69,66 @@ if (age_class == "sapling"){y <- 10
 }
 y
 
+##Alternative conditioning
+
+veg_type <- "grass"
+volume <- 16.08
+if (veg_type == "tree") {
+  mass <- 2.65 * volume^0.9
+} else if (veg_type == "grass") {
+  mass <- 0.65 * volume^1.2
+}
+mass
+
+
+veg_type <- "shrub"
+volume <- 16.08
+if (veg_type == "tree") {
+  mass <- 2.65 * volume^0.9
+} else if (veg_type == "grass") {
+  mass <- 0.65 * volume^1.2
+} else {
+  mass <- NA
+}
+mass
+
+##Exercise 3
+##3.1
+age_class <- "seedling"
+if (age_class == "sapling"){ y = 10
+} else if (age_class == "seedling"){y = 5
+}
+y
+
+##3.2
+age_class = "adult"
+if (age_class =="sapling"){y = 10
+} else if (age_class == "seedling") {y = 5
+} else (y = 0)
+y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
